0e488580453329c63ff11204b040ba770d410952
Emmanuel <emmanuelokello294@gmail.com>
2024-03-18 15:41:21 +0300

    completes control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (distance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  city = city.toUpperCase(); // Ensure case-insensitivity
+  return city === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipRating){
   // Write your code here!
+  switch (tipRating.toLowerCase()) {  // Ensure case-insensitivity
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
+}
+
+switch (key) {
+  case value:
+    
+    break;
+
+  default:
+    break;
 }
